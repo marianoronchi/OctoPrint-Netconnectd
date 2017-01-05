d6050361d2165773c4ed264987529f54ef1e1f65
Mariano Ronchi <marianoronchi@users.noreply.github.com>
2017-01-05 02:45:58 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ import setuptools
 import octoprint_setuptools
 
 setuptools.setup(**octoprint_setuptools.create_plugin_setup_parameters(
-	identifier="netconnectd",
-	name="OctoPrint-Netconnectd",
+	identifier="netconnectdEXO",
+	name="OctoPrint-NetconnectdEXO",
 	version="0.1",
 	description="Client for netconnectd that allows configuration of netconnectd through OctoPrint's settings dialog. It's only available for Linux right now.",
-	author="Gina Häußge",
+	author="Mariano Ronchi",
 	mail="osd@foosel.net",
-	url="http://github.com/OctoPrint/OctoPrint-Netconnectd",
+	url="http://github.com/marianoronchi/OctoPrint-Netconnectd",
 	requires=[
 		"OctoPrint"
 	]
